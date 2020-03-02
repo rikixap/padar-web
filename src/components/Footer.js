5e179bad482d0bb9eb1ae4faf410c9286561856e
rikixap <bara.adityaricky@gmail.com>
2020-03-02 10:00:12 +0700

    fixed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,15 @@
 import React,{Component} from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import '../styles/footer.css'
+import love from '../images/LOGO/love.png'
+
 class Footer extends Component{
     render(){ 
         return(   
             <div className="footer">
                 <div className="wrapper-footer">
                     <div className="text-light text-center">
-                        <p>&copy; 2020 DOES UNIVERSITY All Rights Reserved</p>
+                        <p>Made with <img src={love} style={{width:'3%'}}/> in DOES UNIVERSITY</p>
                     </div>
                     <div className="media">
                         <a target="blank" href="https://www.instagram.com/doesofficial/?hl=id">
