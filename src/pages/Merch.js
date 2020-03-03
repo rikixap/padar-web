e0d305f35039fcdad1cdf9db7de030db858ece6a
apiet <47141162+apietdoes6@users.noreply.github.com>
2020-03-04 05:09:55 +0700

    merch

--- src/pages/Merch.js
+++ src/pages/Merch.js
@@ -1,18 +1,18 @@
 import React,{Component, Fragment} from 'react';
 import Navbar from '../components/Navbar';
 import '../styles/Merch.css';
-import tshirt1 from '../images/MERCH/SHIRT/1.jpg';
-import tshirt2 from '../images/MERCH/SHIRT/2.jpg'
+// import tshirt1 from '../images/MERCH/SHIRT/1.jpg';
+// import tshirt2 from '../images/MERCH/SHIRT/2.jpg'
 import Slider from 'react-slick';
 import Zoom from 'react-medium-image-zoom'
 import 'react-medium-image-zoom/dist/styles.css'
 import Footer from '../components/Footer';
-import boxset1 from '../images/MERCH/1233.png';
-import boxset2 from '../images/MERCH/1 (3).png';
-import boxset3 from '../images/MERCH/2 (1).png';
-import boxset4 from '../images/MERCH/2 (2).png';
-import boxset5 from '../images/MERCH/2 (3).png';
-import boxset6 from '../images/MERCH/2.png';
+// import boxset1 from '../images/MERCH/1233.png';
+// import boxset2 from '../images/MERCH/1 (3).png';
+// import boxset3 from '../images/MERCH/2 (1).png';
+// import boxset4 from '../images/MERCH/2 (2).png';
+// import boxset5 from '../images/MERCH/2 (3).png';
+// import boxset6 from '../images/MERCH/2.png';
 
 var settings = {
     dots: true,
@@ -42,7 +42,7 @@ class Merch extends Component{
                                     <Zoom>
                                         <img
                                         alt="that wanaka tree"
-                                        src={tshirt1}
+                                        src={'https://res.cloudinary.com/divsysf5b/image/upload/v1583243896/images/2_yj8i9u.jpg'}
                                         width="200"
                                         style={{top:'40px'}}
                                         className="img-merch"
@@ -53,7 +53,7 @@ class Merch extends Component{
                                     <Zoom>
                                         <img
                                         alt="that wanaka tree"
-                                        src={tshirt2}
+                                        src={'https://res.cloudinary.com/divsysf5b/image/upload/v1583243899/images/1_l3ftra.jpg'}
                                         width="200"
                                         style={{top:'40px'}}
                                         className="img-merch"
@@ -96,7 +96,7 @@ class Merch extends Component{
                                     <Zoom>
                                         <img
                                         alt="that wanaka tree"
-                                        src={boxset1}
+                                        src={'https://res.cloudinary.com/divsysf5b/image/upload/v1583272801/1_3_-min_2_hcwfwm.png'}
                                         width="200"
                                         style={{top:'40px'}}
                                         className="img-merch"
@@ -107,7 +107,7 @@ class Merch extends Component{
                                     <Zoom>
                                         <img
                                         alt="that wanaka tree"
-                                        src={boxset2}
+                                        src={'https://res.cloudinary.com/divsysf5b/image/upload/v1583272997/1-min_pffwqr.png'}
                                         width="200"
                                         style={{top:'40px'}}
                                         className="img-merch"
@@ -118,7 +118,7 @@ class Merch extends Component{
                                     <Zoom>
                                         <img
                                         alt="that wanaka tree"
-                                        src={boxset3}
+                                        src={'https://res.cloudinary.com/divsysf5b/image/upload/v1583273037/2_1_-min_d1gvwb.png'}
                                         width="200"
                                         style={{top:'40px'}}
                                         className="img-merch"
@@ -129,7 +129,7 @@ class Merch extends Component{
                                     <Zoom>
                                         <img
                                         alt="that wanaka tree"
-                                        src={boxset4}
+                                        src={'https://res.cloudinary.com/divsysf5b/image/upload/v1583273082/2_3_-min_fyvmok.png'}
                                         width="200"
                                         style={{top:'40px'}}
                                         className="img-merch"
@@ -140,7 +140,7 @@ class Merch extends Component{
                                     <Zoom>
                                         <img
                                         alt="that wanaka tree"
-                                        src={boxset5}
+                                        src={'https://res.cloudinary.com/divsysf5b/image/upload/v1583273126/2-min_zsfgor.png'}
                                         width="200"
                                         style={{top:'40px'}}
                                         className="img-merch"
@@ -151,13 +151,24 @@ class Merch extends Component{
                                     <Zoom>
                                         <img
                                         alt="that wanaka tree"
-                                        src={boxset6}
+                                        src={'https://res.cloudinary.com/divsysf5b/image/upload/v1583273139/1233-min_bvkgwv.png'}
                                         width="200"
                                         style={{top:'40px'}}
                                         className="img-merch"
                                         />
                                     </Zoom>
                                 </div>
+                                <div className="img1">
+                                <Zoom>
+                                    <img
+                                    alt="that wanaka tree"
+                                    src={'https://res.cloudinary.com/divsysf5b/image/upload/v1583273239/2_2_-min_aplzl4.png'}
+                                    width="200"
+                                    style={{top:'40px'}}
+                                    className="img-merch"
+                                    />
+                                </Zoom>
+                            </div>
                             </Slider>   
                             </div>
                         </div>
